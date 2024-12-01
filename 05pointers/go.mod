4ed600b5f88c700c26ab25f1066fce979c81dc6b
KoZin <zinminthu.dev@gmail.com>
2024-12-01 20:27:13 +0700

    learn pointer values

---
+++ 05pointers/go.mod
@@ -0,0 +1,3 @@
+module github.com/zin-min-thu/pointers
+
+go 1.23.0
---
+++ 05pointers/pointers.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 32 // regular variable
+
+	// we can declare
+	// var agePointer *int
+	agePointer := &age
+
+	fmt.Println("Age: ", age)
+	fmt.Println("Age Pointer: ", agePointer)
+	// if you want to get value behind the pointer
+	fmt.Println("Age Pointer Value: ", *agePointer)
+
+	// if we use age in different function, go will copy of the value and use two momery address
+	adultYears := getAdultYears(age)
+	fmt.Println(adultYears)
+
+	// testing default empty variables pointer
+	var intVal int
+	var floatVal float64
+	var stringVal string
+
+	intPointer := &intVal
+	floatPointer := &floatVal
+	stringPointer := &stringVal
+
+	fmt.Println("Int pointer: ", &intPointer)
+	fmt.Println("Int pointer value: ", *intPointer)
+	fmt.Println("Float pointer: ", &floatPointer)
+	fmt.Println("Float pointer value: ", *floatPointer)
+	fmt.Println("String pointer: ", &stringPointer)
+	fmt.Println("String pointer value: ", *stringPointer)
+
+}
+
+func getAdultYears(age int) int {
+	return age - 18
+}
