38e802f0de4b48ea7d9ac24e024c835bc59af39a
KoZin <zinminthu.dev@gmail.com>
2024-12-02 21:28:02 +0700

    learn dereference pointer value

--- 05pointers/pointers.go
+++ 05pointers/pointers.go
@@ -15,27 +15,37 @@ func main() {
 	fmt.Println("Age Pointer Value: ", *agePointer)
 
 	// if we use age in different function, go will copy of the value and use two momery address
-	adultYears := getAdultYears(age)
-	fmt.Println(adultYears)
+	// adultYears := getAdultYears(agePointer)
+	// fmt.Println(adultYears)
+
+	editAgeToAdultYears(agePointer)
+	fmt.Println("Dereference Age: ", age)
 
 	// testing default empty variables pointer
-	var intVal int
-	var floatVal float64
-	var stringVal string
+	/*
+		var intVal int
+		var floatVal float64
+		var stringVal string
 
-	intPointer := &intVal
-	floatPointer := &floatVal
-	stringPointer := &stringVal
+		intPointer := &intVal
+		floatPointer := &floatVal
+		stringPointer := &stringVal
 
-	fmt.Println("Int pointer: ", &intPointer)
-	fmt.Println("Int pointer value: ", *intPointer)
-	fmt.Println("Float pointer: ", &floatPointer)
-	fmt.Println("Float pointer value: ", *floatPointer)
-	fmt.Println("String pointer: ", &stringPointer)
-	fmt.Println("String pointer value: ", *stringPointer)
+		fmt.Println("Int pointer: ", &intPointer)
+		fmt.Println("Int pointer value: ", *intPointer)
+		fmt.Println("Float pointer: ", &floatPointer)
+		fmt.Println("Float pointer value: ", *floatPointer)
+		fmt.Println("String pointer: ", &stringPointer)
+		fmt.Println("String pointer value: ", *stringPointer)
+	*/
 
 }
 
-func getAdultYears(age int) int {
-	return age - 18
+// func getAdultYears(age *int) int {
+// 	return *age - 18
+// }
+
+// deference pointer
+func editAgeToAdultYears(age *int) {
+	*age = *age - 18
 }
