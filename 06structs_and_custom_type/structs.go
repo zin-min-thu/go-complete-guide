a43b27549d79d71b0ad4a9a2e05222d2aa20083f
KoZin <zinminthu.dev@gmail.com>
2024-12-03 22:59:20 +0700

    learn mutation method with pointer

--- 06structs_and_custom_type/structs.go
+++ 06structs_and_custom_type/structs.go
@@ -12,19 +12,35 @@ type user struct {
 	createdAt time.Time
 }
 
+// using receiver methods with struct
+func (u *user) outputUserDetails() {
+	fmt.Printf("First Name: %v\nLast Name: %v\nDOB: %v\n", u.firstName, u.lastName, u.birthDate)
+}
+
+// mutation methods with pointer receiver
+func (u *user) clearUserName() {
+	u.firstName = ""
+	u.lastName = ""
+}
+
 func main() {
 	userFirstName := getUserData("Enter first name: ")
 	userLastName := getUserData("Enter last name: ")
 	userBirthdate := getUserData("Enter birthdate (MM/DD/YYY): ")
 
 	appUser := user{}
-	// long form
+
 	appUser = user{
 		firstName: userFirstName,
 		lastName:  userLastName,
 		birthDate: userBirthdate,
 		createdAt: time.Now(),
 	}
+
+	appUser.outputUserDetails()
+	appUser.clearUserName()
+	appUser.outputUserDetails()
+
 	// sort form must be same order with user struct
 	// appUser = user{
 	// 	userFirstName,
@@ -34,13 +50,14 @@ func main() {
 	// }
 
 	// outputUser(appUser) // without pointer will copy appUser when call then store new memory address
-	outputUser(&appUser)
+	// outputUser(&appUser)
 }
 
-func outputUser(u *user) {
-	// fmt.Printf("First Name: %v\nLast Name: %v\nDOB: %v\n", firstName, lastName, birthdate)
-	fmt.Printf("First Name: %v\nLast Name: %v\nDOB: %v\n", u.firstName, u.lastName, u.birthDate)
-}
+// using pointer
+// func outputUser(u *user) {
+// 	// fmt.Printf("First Name: %v\nLast Name: %v\nDOB: %v\n", firstName, lastName, birthdate)
+// 	fmt.Printf("First Name: %v\nLast Name: %v\nDOB: %v\n", u.firstName, u.lastName, u.birthDate)
+// }
 
 func getUserData(promptText string) string {
 	fmt.Print(promptText)
