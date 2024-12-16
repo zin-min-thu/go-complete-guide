9d779259b88782e5b5f21df7aea5619753adfea7
KoZin <zinminthu.dev@gmail.com>
2024-12-16 22:19:19 +0700

    using maps and mutating

--- 11array_slice_map/lists/lists.go
+++ 11array_slice_map/lists/lists.go
@@ -1,4 +1,4 @@
-package main
+package lists
 
 import "fmt"
 
@@ -13,6 +13,10 @@ func main() {
 
 	prices[1] = 9.99
 	prices = append(prices, 5.99)
+	fmt.Println(prices)
+
+	discountPrices := []float64{101.99, 80.99, 20.59}
+	prices = append(prices, discountPrices...)
 	fmt.Println(prices)
 }
 
---
+++ 11array_slice_map/maps.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// websites := []string{"https://google.com", "https://aws.com"}
+	websites := map[string]string{
+		"Google":              "https://google.com",
+		"Amazon Web Services": "https://aws.com",
+	}
+
+	fmt.Println(websites)
+	fmt.Println(websites["Amazon Web Services"])
+
+	// add new
+	websites["LinkedIn"] = "https://linkedin.com"
+	fmt.Println(websites)
+
+	delete(websites, "Google")
+	fmt.Println(websites)
+
+}
