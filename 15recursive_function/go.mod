81cfc39d33cc60b93b8868eeabe2f25da36654cf
KoZin <zinminthu.dev@gmail.com>
2024-12-18 22:29:21 +0700

    learn recursive function

---
+++ 15recursive_function/go.mod
@@ -0,0 +1,3 @@
+module github.com/zin-min-thurecursion
+
+go 1.23.0
---
+++ 15recursive_function/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fact := factorial(5)
+	fmt.Println(fact)
+}
+
+func factorial(number int) int {
+
+	// recursive function
+	if number == 0 {
+		return 1
+	}
+	return number * factorial(number-1)
+
+	// simple code
+	// result := 1
+	// for i := 1; i <= number; i++ {
+	// 	result = result * i
+	// }
+
+	// return result
+}
+
+// factorial of 5 => 5 * 4 * 3 * 2 * 1 => 120
