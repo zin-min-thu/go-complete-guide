2d860472c6a798c364f608c3fbbeac5a7dae5bf4
KoZin <zinminthu.dev@gmail.com>
2025-01-02 23:00:45 +0700

    update event api

--- 19REST_api_sql_project_with_auth/routes/routes.go
+++ 19REST_api_sql_project_with_auth/routes/routes.go
@@ -9,4 +9,5 @@ func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
 	server.POST("/events", createEvent)
 	server.GET("/events/:id", getEvent)
+	server.PUT("/events/:id", updateEvent)
 }
