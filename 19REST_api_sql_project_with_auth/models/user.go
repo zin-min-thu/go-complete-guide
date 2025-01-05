94b51008af0bf3be979b6864f072e6abb53a8390
KoZin <zinminthu.dev@gmail.com>
2025-01-05 22:47:02 +0700

    hashing password

--- 19REST_api_sql_project_with_auth/models/user.go
+++ 19REST_api_sql_project_with_auth/models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/zin-min-thu/apisqlprojectwithauth/db"
+import (
+	"github.com/zin-min-thu/apisqlprojectwithauth/db"
+	"github.com/zin-min-thu/apisqlprojectwithauth/utils"
+)
 
 type User struct {
 	ID       int64
@@ -19,7 +22,13 @@ func (u User) Save() error {
 
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassword(u.Password)
+
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
 
 	if err != nil {
 		return err
