4661a577945c10cc51eb03e606888b1a68088399
KoZin <zinminthu.dev@gmail.com>
2025-01-06 22:52:29 +0700

    generating jwt token

--- 19REST_api_sql_project_with_auth/routes/users.go
+++ 19REST_api_sql_project_with_auth/routes/users.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/zin-min-thu/apisqlprojectwithauth/models"
+	"github.com/zin-min-thu/apisqlprojectwithauth/utils"
 )
 
 func signup(context *gin.Context) {
@@ -43,5 +44,12 @@ func login(context *gin.Context) {
 		return
 	}
 
-	context.JSON(http.StatusOK, gin.H{"message": "Login successful"})
+	token, err := utils.GenerateToken(user.Email, user.ID)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not authenticate user"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login successful", "token": token})
 }
