379836b8f22b11a9528e3dde42315b208ad85b4d
KoZin <zinminthu.dev@gmail.com>
2025-01-08 22:53:45 +0700

    restrict middleware for add and delete event

--- 19REST_api_sql_project_with_auth/routes/events.go
+++ 19REST_api_sql_project_with_auth/routes/events.go
@@ -42,20 +42,6 @@ func getEvent(context *gin.Context) {
 
 func createEvent(context *gin.Context) {
 
-	// token := context.Request.Header.Get("Authorization")
-
-	// if token == "" {
-	// 	context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
-	// 	return
-	// }
-
-	// userId, err := utils.VerifyToken(token)
-
-	// if err != nil {
-	// 	context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
-	// 	return
-	// }
-
 	var event models.Event
 	err := context.ShouldBindJSON(&event)
 
@@ -86,10 +72,16 @@ func updateEvent(context *gin.Context) {
 		return
 	}
 
-	_, err = models.GetEventByID(eventId)
+	userId := context.GetInt64("userId")
+	event, err := models.GetEventByID(eventId)
 
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+	}
+
+	if event.UserID != userId {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to update event"})
 		return
 	}
 
@@ -111,7 +103,7 @@ func updateEvent(context *gin.Context) {
 		return
 	}
 
-	context.JSON(http.StatusOK, gin.H{"message": "Event updated successufl.", "event": updatedEvent})
+	context.JSON(http.StatusOK, gin.H{"message": "Event updated successufl."})
 }
 
 func deleteEvent(context *gin.Context) {
@@ -122,10 +114,16 @@ func deleteEvent(context *gin.Context) {
 		return
 	}
 
+	userId := context.GetInt64("userId")
 	event, err := models.GetEventByID(eventId)
 
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event."})
+		return
+	}
+
+	if event.UserID != userId {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to delete event"})
 		return
 	}
 
